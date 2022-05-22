5f57ac0bfd0a3f7eff210c1fd7c8e42997c9cf36
Orhan Ozbasaran <73716961+Obihan98@users.noreply.github.com>
2022-05-21 18:23:24 -0700

    Reset Button and Game Status and Alerts Added

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 // Setting up the board
 var board = document.querySelector(".game-board");
-var boardHTML = "";
+var boardHTML;
+var blocks;
 
 var number_of_rows = 3;
 var number_of_cols = 3;
-var boardArray = new Array(number_of_rows);
+var boardArray;
 
 function setBoard(){
+    boardHTML = "";
     boardArray = new Array(number_of_rows);
     for (let i = 0; i < number_of_rows; i++){
         boardHTML += '<div class="row">';
@@ -18,17 +20,19 @@ function setBoard(){
         boardHTML += '</div>';
     }
     board.innerHTML = boardHTML;
+    blocks = document.querySelectorAll(".block");
 }
-
 setBoard();
 
 // Event Listener Clicking on Board
-var blocks = document.querySelectorAll(".block");
-for (let i = 0; i < blocks.length; i++){
-    blocks[i].addEventListener("click", function(){
-        handleClick(blocks[i].getAttribute("id"), board);
-    })
+function setListeners(){
+    for (let i = 0; i < blocks.length; i++){
+        blocks[i].addEventListener("click", function(){
+            handleClick(blocks[i].getAttribute("id"), board);
+        });
+    }
 }
+setListeners();
 
 // Putting X or O to the board
 var turn_count = 0;
@@ -43,18 +47,24 @@ function handleClick(id){
         if (turn_count % 2 == 0 && notClicked(x, y, boardArray)){
             document.querySelector("#" + id + " img").setAttribute("src", "x.png");
             boardArray[x][y] = 1;
+            document.querySelector("#status").innerHTML = "Turn: O";
             turn_count++;
         } else if (turn_count % 2 == 1 && notClicked(x, y, boardArray)){
             document.querySelector("#" + id + " img").setAttribute("src", "o.png");
             boardArray[x][y] = -1;
+            document.querySelector("#status").innerHTML = "Turn: X";
             turn_count++;
         }
     }
     // When there is a winner
-    else{
-
-    }
     winner = checkWinner(); 
+    if (winner == 1){
+        document.querySelector("#status").innerHTML = "Winner: X!";
+    } else if (winner == -1){
+        document.querySelector("#status").innerHTML = "Winner: O!";
+    } else if (turn_count == 9){
+        document.querySelector("#status").innerHTML = "Tie!";
+    }
 }
 
 function notClicked(x, y, boardArray){
@@ -74,7 +84,7 @@ function checkWinner(){
         })){
             return 1;
         } else if (boardArray[i].every(element => {
-            if (element == 2) return true;
+            if (element == -1) return true;
         })){
             return -1;
         }
@@ -101,3 +111,12 @@ function checkWinner(){
     else if(boardArray[2][0] + boardArray[1][1] + boardArray[0][2] === -3){return -1;}
     return 0;
 }
+
+// Event Listener for Resetting the Board
+document.querySelector("#reset-button").addEventListener("click", function(){
+    setBoard();
+    setListeners();
+    document.querySelector("#status").innerHTML = "Turn: X";
+    turn_count = 0;
+    winner = 0;
+});
